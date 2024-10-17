e3ae4f9aec9974eaf8fcfd87d4fb50d456e8e250
Fine <posuklouf@gmail.com>
2024-10-17 23:37:03 +0800

    feat: sitemap 增加国际化

--- app/routes/[sitemap.xml].tsx
+++ app/routes/[sitemap.xml].tsx
@@ -1,36 +1,78 @@
+import type { LoaderFunctionArgs } from "@remix-run/cloudflare";
+import { locales, defaultLocale } from "~/i18n";
+
 const sitemaps = [
   {
-    loc: "https://miraibogo.org",
+    path: "/",
+    withI18N: true,
     lastmod: new Date().toISOString(),
     priority: "1.0",
   },
   {
-    loc: "https://miraibogo.org/privacy-policy",
-    lastmod: new Date('2024-10-15').toISOString(),
+    path: "/privacy-policy",
+    withI18N: false,
+    lastmod: new Date("2024-10-15").toISOString(),
     priority: "0.9",
   },
   {
-    loc: "https://miraibogo.org/terms-of-service",
-    lastmod: new Date('2024-10-15').toISOString(),
+    path: "/terms-of-service",
+    withI18N: false,
+    lastmod: new Date("2024-10-15").toISOString(),
     priority: "0.9",
   },
 ];
 
-export const loader = () => {
+export const loader = async ({ request, params }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+
+  const sitemapList = [] as {
+    loc: String;
+    lastmod: String;
+    priority: String;
+  }[];
+
+  sitemaps.forEach((site) => {
+    const pathname = site.path.startsWith("/") ? site.path.slice(1) : site.path;
+
+    if (site.withI18N) {
+      locales.forEach((locale) => {
+        const loc = [url.origin];
+
+        if (locale !== defaultLocale) loc.push(locale);
+
+        if (pathname) loc.push(pathname);
+
+        sitemapList.push({
+          loc: loc.join("/"),
+          lastmod: site.lastmod,
+          priority: site.priority,
+        });
+      });
+    } else {
+      sitemapList.push({
+        loc: pathname ? [url.origin, pathname].join("/") : url.origin,
+        lastmod: site.lastmod,
+        priority: site.priority,
+      });
+    }
+  });
+
   const content = `
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${sitemaps.map(
-          (site) => `
+        ${sitemapList
+          .map((site) => {
+            return `
             <url>
               <loc>${site.loc}</loc>
               <lastmod>${site.lastmod}</lastmod>
               <priority>${site.priority}</priority>
             </url>
-          `
-        )}
+          `;
+          })
+          .join("\n")}
       </urlset>
       `;
-  
+
   return new Response(content, {
     status: 200,
     headers: {
